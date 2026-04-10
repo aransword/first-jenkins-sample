0d1c226da12876acd2c3482156f649484c57eb63
aransword <lupin521@gmail.com>
2026-04-10 10:08:11 +0900

    push1

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello() {
         return "Hello, Jenkins CI/CD!";
     }
+
+    @GetMapping("/test")
+    public String jenkinsTest() {
+        return "Test Success";
+    }
 }
